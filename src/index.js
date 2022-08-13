32ae05ce3f992f6de95b3b2dbf96145c058d1c87
Andrewz256 <punkz666@gmail.com>
2022-08-13 13:43:02 +0300

    done.morse

--- src/index.js
+++ src/index.js
@@ -38,7 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arrExpr = expr.split('');
+    let result = [];
+    
+    for (var i = 0; i < arrExpr.length; i += 10) {
+        let fletter = (arrExpr.slice(i, i + 10)).join('');
+        let symb = [];
+        let arrLet = fletter;
+        
+            if (arrLet === "**********") {
+                symb.push(" ");
+            } else {
+                let letter = deleteZeroes(fletter);
+                arrLet = letter.split('');
+                
+                for (var j = 0; j < arrLet.length; j += 2) {
+            
+                    if (arrLet.slice(j, j + 2).join('') === '10' ) {
+
+                        symb.push('.')} 
+                    if (arrLet.slice(j, j + 2).join('') === '11' ) {
+                        symb.push('-')}        
+                }
+            } 
+
+            r=MORSE_TABLE[symb.join('')];
+            
+            if (r === undefined) {
+                result.push(" ");
+            } else {
+                result.push(r);
+            }
+            
+
+        }
+    
+    let strResult = result.join('');
+    return strResult;
+        
+}
+
+
+function deleteZeroes(str1) {
+    return str1.substr(str1.indexOf("1") , str1.length);
 }
 
 module.exports = {
